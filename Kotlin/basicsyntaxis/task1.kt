c468ec3acc9dcf12005c255273ad760d487200fb
sup0rt <georgal0527@gmail.com>
2025-02-12 14:53:44 +0300

    asic functions

--- Kotlin/basicsyntaxis/task1.kt
+++ Kotlin/basicsyntaxis/task1.kt
@@ -1,12 +1,12 @@
 package Kotlin.basicsyntaxis
 
 fun main() {
-    val person1 = Person("Alice", 30, "Female")
-    person1.displayInfo()
-    person1.increaseAge()
-    person1.displayInfo()
-    person1.changeName("Alice Smith")
-    person1.displayInfo()
+        val person1 = Person("Alice", 30, "Female")
+        person1.displayInfo()
+        person1.increaseAge()
+        person1.displayInfo()
+        person1.changeName("Alice Smith")
+        person1.displayInfo()
 }
 
 open class Person(var name: String, var age: Int, var gender: String) {
